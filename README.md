638ab89c90979784605903b196bee56a9d99d8e5
Ciaron Linstead <linstead@pik-potsdam.de>
2014-05-16 17:01:03 +0200

    works in a loop, n*r constant

--- README.md
+++
@@ -1,4 +0,0 @@
-cpubench
-========
-
-CPU Benchmark 
--- cpubench.c
+++ cpubench.c
@@ -19,52 +19,74 @@ dummy(double *a, double *b, double *c, double *d) {
 int 
 main(int argc, char **argv) {
 
+    int max_n = 1000000; /* n should never exceed this, to avoid running out of system memory */
     /* choose r and n such that overall execution time stays roughly constant */
-    int n = 175000000; // array size
-    int r = 5; // number of test repetitions
+    int n = 0; //18750000; // array size
+    int r; // number of test repetitions
     int i, j; // loop counters
+    int step = 1;
     double *a, *b, *c, *d; // arrays
     struct timespec start, stop; // start and stop timers (seconds)
     double accum;
     double mflops;
 
-    /* malloc the data arrays */
-    a = (double *)malloc(n * sizeof(double));
-    b = (double *)malloc(n * sizeof(double));
-    c = (double *)malloc(n * sizeof(double));
-    d = (double *)malloc(n * sizeof(double));
+    //printf("n\taccum\tmflops\n"); 
+    printf("n\tmflops\n"); 
 
-    /* initialise b, c and d */
-    for (i=0; i<n; i++) {
-        b[i] = 1.0;
-        c[i] = 2.0;
-        d[i] = 3.0;
-    }
+    while (n < max_n) {
 
-    clock_gettime(CLOCK_REALTIME, &start);
+        if (n >= 100) step = 10;
+        if (n >= 1000) step = 10;
+        if (n >= 10000) step = 100;
+        if (n >= 100000) step = 1000;
+        if (n >= 300000) step = 10000;
 
-    /* MAIN LOOP */
-    for (j=0; j<r; j++) {
+        n += step;
+        //r = 187500000 / n;
+        r = 18750000 / n;
+
+        /* malloc the data arrays */
+        if (!(a = (double *)malloc(n * sizeof(double)))) { printf("malloc failed (a)\n"); return 1; }
+        if (!(b = (double *)malloc(n * sizeof(double)))) { printf("malloc failed (b)\n"); return 1; }
+        if (!(c = (double *)malloc(n * sizeof(double)))) { printf("malloc failed (c)\n"); return 1; }
+        if (!(d = (double *)malloc(n * sizeof(double)))) { printf("malloc failed (d)\n"); return 1; }
+
+        /* initialise b, c and d */
         for (i=0; i<n; i++) {
-            a[i] = b[i] + c[i] * d[i];
+            b[i] = 1.0;
+            c[i] = 2.0;
+            d[i] = 3.0;
         }
-        /* prevent loop interchange by optimising compiler */
-        if (a[2] < 0) {
-            dummy(a,b,c,d);
+
+        /* start timer */
+        clock_gettime(CLOCK_REALTIME, &start);
+
+        /* MAIN LOOP */
+        for (j=0; j<r; j++) {
+            for (i=0; i<n; i++) {
+                a[i] = b[i] + c[i] * d[i];
+            }
+            /* prevent loop interchange by optimising compiler */
+            if (a[2] < 0) {
+                dummy(a,b,c,d);
+            }
         }
-    }
 
-    clock_gettime(CLOCK_REALTIME, &stop);
+        /* stop timer */
+        clock_gettime(CLOCK_REALTIME, &stop);
 
-    accum = ( stop.tv_sec - start.tv_sec )
-          + ( stop.tv_nsec - start.tv_nsec )
-            / (double) 1e9;
-    mflops = r * n * 2.0 / accum / 1e6;
+        accum = ( stop.tv_sec - start.tv_sec )
+              + ( stop.tv_nsec - start.tv_nsec )
+                / (double) 1e9;
 
-    printf("Elapsed time (seconds): %f\n", accum);
-    printf("Rate (MFLOPS/second): %f\n", mflops);
+        mflops = r * n * 2.0 / accum / 1e6;
 
-    free(a); free(b); free(c); free(d);
+        //printf("%d\t%f\t%f\n", n, accum, mflops); 
+        printf("%d\t%f\n", n, mflops); 
+
+        free(a); free(b); free(c); free(d);
+
+    } // while
 
     return 0;
 }
